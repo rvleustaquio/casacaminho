4d26289c1237136a2616dc25e7f03c78a856f33c
Rodrigo Eustáquio <rvleustaquio@gmail.com>
2025-05-28 22:51:04 -0300

    fix: better docker files

--- frontend/src/app/features/estado-civil/estado-civil.component.ts
+++ frontend/src/app/features/estado-civil/estado-civil.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 
 @Component({
   selector: 'app-estado-civil',
-  imports: [CardModule],
+  imports: [CardModule, TableModule, ButtonModule, ToggleButtonModule],
   templateUrl: './estado-civil.component.html',
 })
-export class EstadoCivilComponent {}
+export class EstadoCivilComponent {
+  // Layout properties
+  header = 'Estados Civis';
+
+  // Data properties
+  data = [
+    {
+      name: 'Solteiro',
+    },
+    {
+      name: 'Casado',
+    },
+    {
+      name: 'Divorciado',
+    },
+    {
+      name: 'Viúvo',
+    },
+    {
+      name: 'Separado',
+    },
+    {
+      name: 'União Estável',
+    },
+  ];
+}
